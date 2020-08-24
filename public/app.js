be3c5ce50e87df940816f29a9cdb5c7568bd8130
itokanade <56240087+itokanade@users.noreply.github.com>
2020-08-24 15:39:46 +0900

    commit

---
+++ public/app.js
@@ -0,0 +1,296 @@
+//自分自身の情報を入れる箱
+const IAM = {
+  token: null,    // トークン
+  name: null,     // 名前
+  is_join: false  // 入室中？
+};
+
+// メンバー一覧を入れる箱
+const MEMBER = {
+  0: "マスター"
+};
+
+// Socket.ioのクライアント用オブジェクトをセット
+const socket = io();
+
+
+//-------------------------------------
+// STEP1. Socket.ioサーバへ接続
+//-------------------------------------
+/**
+ * [イベント] トークンが発行されたら
+ */
+socket.on("token", (data)=>{
+  // トークンを保存
+  IAM.token = data.token;
+
+  // 表示を切り替える
+  if( ! IAM.is_join ){
+    $("#nowconnecting").style.display = "none";   // 「接続中」を非表示
+    $("#inputmyname").style.display = "block";    // 名前入力を表示
+    $("#txt-myname").focus();
+  }
+});
+
+//-------------------------------------
+// STEP2. 名前の入力
+//-------------------------------------
+/**
+ * [イベント] 名前入力フォームが送信された
+ */
+$("#frm-myname").addEventListener("submit", (e)=>{
+  // 規定の送信処理をキャンセル(画面遷移しないなど)
+  e.preventDefault();
+
+  // 入力内容を取得する
+  const myname = $("#txt-myname");
+  if( myname.value === "" ){
+    return(false);
+  }
+
+  // 名前をセット
+  $("#myname").innerHTML = myname.value;
+  IAM.name = myname.value;
+
+  // Socket.ioサーバへ送信
+  socket.emit("join", {token:IAM.token, name:IAM.name});
+
+  // ボタンを無効にする
+  $("#frm-myname button").setAttribute("disabled", "disabled");
+});
+
+/**
+ * [イベント] 入室結果が返ってきた
+ */
+socket.on("join-result", (data)=>{
+  //------------------------
+  // 正常に入室できた
+  //------------------------
+  if( data.status ){
+    // 入室フラグを立てる
+    IAM.is_join = true;
+
+    // すでにログイン中のメンバー一覧を反映
+    for(let i=0; i<data.list.length; i++){
+      const cur = data.list[i];
+      if( ! (cur.token in MEMBER) ){
+        addMemberList(cur.token, cur.name);
+      }
+    }
+
+    // 表示を切り替える
+    $("#inputmyname").style.display = "none";   // 名前入力を非表示
+    $("#chat").style.display = "block";         // チャットを表示
+    $("#msg").focus();
+  }
+  //------------------------
+  // できなかった
+  //------------------------
+  else{
+    alert("入室できませんでした");
+  }
+
+  // ボタンを有効に戻す
+  $("#frm-myname button").removeAttribute("disabled");
+});
+
+//-------------------------------------
+// STEP3. チャット開始
+//-------------------------------------
+/**
+ * [イベント] 発言フォームが送信された
+ */
+$("#frm-post").addEventListener("submit", (e)=>{
+  // 規定の送信処理をキャンセル(画面遷移しないなど)
+  e.preventDefault();
+
+  // 入力内容を取得する
+  const msg = $("#msg");
+  if( msg.value === "" ){
+    return(false);
+  }
+
+  // Socket.ioサーバへ送信
+  socket.emit("post", {text: msg.value, token:IAM.token});
+
+  // 発言フォームを空にする
+  msg.value = "";
+});
+
+/**
+ * [イベント] 退室ボタンが押された
+ */
+$("#frm-quit").addEventListener("submit", (e)=>{
+  // 規定の送信処理をキャンセル(画面遷移しないなど)
+  e.preventDefault();
+
+  if( confirm("本当に退室しますか？") ){
+    // Socket.ioサーバへ送信
+    socket.emit("quit", {token:IAM.token});
+
+    // ボタンを無効にする
+    $("#frm-quit button").setAttribute("disabled", "disabled");
+  }
+});
+
+/**
+ * [イベント] 退室処理の結果が返ってきた
+ */
+socket.on("quit-result", (data)=>{
+  if( data.status ){
+    gotoSTEP1();
+  }
+  else{
+    alert("退室できませんでした");
+  }
+
+  // ボタンを有効に戻す
+  $("#frm-quit button").removeAttribute("disabled");
+});
+
+/**
+ * [イベント] 誰かが入室した
+ */
+socket.on("member-join", (data)=>{
+  if( IAM.is_join ){
+    addMessageFromMaster(`${data.name}さんが入室しました`);
+    addMemberList(data.token, data.name);
+  }
+});
+
+/**
+ * [イベント] 誰かが退室した
+ */
+socket.on("member-quit", (data)=>{
+  if( IAM.is_join ){
+    const name = MEMBER[data.token];
+    addMessageFromMaster(`${name}さんが退室しました`);
+    removeMemberList(data.token);
+  }
+});
+
+/**
+ * [イベント] 誰かが発言した
+ */
+socket.on("member-post", (msg)=>{
+  if( IAM.is_join ){
+    const is_me = (msg.token === IAM.token);
+    addMessage(msg, is_me);
+  }
+});
+
+
+/**
+ * 最初の状態にもどす
+ *
+ * @return {void}
+ */
+function gotoSTEP1(){
+  // NowLoadingから開始
+  $("#nowconnecting").style.display = "block";  // NowLoadingを表示
+  $("#inputmyname").style.display = "none";     // 名前入力を非表示
+  $("#chat").style.display = "none";            // チャットを非表示
+
+  // 自分の情報を初期化
+  IAM.token = null;
+  IAM.name  = null;
+  IAM.is_join = false;
+
+  // メンバー一覧を初期化
+  for( let key in MEMBER ){
+    if( key !== "0" ){
+      delete MEMBER[key];
+    }
+  }
+
+  // チャット内容を全て消す
+  $("#txt-myname").value = "";     // 名前入力欄 STEP2
+  $("#myname").innerHTML = "";     // 名前表示欄 STEP3
+  $("#msg").value = "";            // 発言入力欄 STEP3
+  $("#msglist").innerHTML = "";    // 発言リスト STEP3
+  $("#memberlist").innerHTML = ""; // メンバーリスト STEP3
+
+  // Socket.ioサーバへ再接続
+  socket.close().open();
+}
+
+/**
+ * 発言を表示する
+ *
+ * @param {object}  msg - {token:"abcd", name:"foo"}
+ * @param {boolean} [is_me=false]
+ * @return {void}
+ */
+function addMessage(msg, is_me=false){
+  const list = $("#msglist");
+  const li = document.createElement("li");
+  const name = MEMBER[msg.token];
+
+  // マスターの発言
+  if( msg.token === 0 ){
+    li.innerHTML = `<span class="msg-master"><span class="name">${name}</span>> ${msg.text}</span>`;
+  }
+  // 自分の発言
+  else if( is_me ){
+    li.innerHTML = `<span class="msg-me"><span class="name">${name}</span>> ${msg.text}</span>`;
+  }
+  // それ以外の発言
+  else{
+    li.innerHTML = `<span class="msg-member"><span class="name">${name}</span>> ${msg.text}</span>`;
+  }
+
+  // リストの最初に追加
+  list.insertBefore(li, list.firstChild);
+}
+
+/**
+ * チャットマスターの発言
+ *
+ * @param {string} msg
+ * @return {void}
+ */
+function addMessageFromMaster(msg){
+  addMessage({token: 0, text: msg});
+}
+
+
+/**
+ * メンバーリストに追加
+ *
+ * @param {string} token
+ * @param {string} name
+ * @return {void}
+ */
+function addMemberList(token, name){
+  const list = $("#memberlist");
+  const li = document.createElement("li");
+  li.setAttribute("id", `member-${token}`);
+  if( token == IAM.token ){
+    li.innerHTML = `<span class="member-me">${name}</span>`;
+  }
+  else{
+    li.innerHTML = name;
+  }
+
+  // リストの最後に追加
+  list.appendChild(li);
+
+  // 内部変数に保存
+  MEMBER[token] = name;
+}
+
+/**
+ * メンバーリストから削除
+ *
+ * @param {string} token
+ * @return {void}
+ */
+function removeMemberList(token){
+  const id = `#member-${token}`;
+  if( $(id) !== null ){
+    $(id).parentNode.removeChild( $(id) );
+  }
+
+  // 内部変数から削除
+  delete MEMBER[token];
+}
---
+++ serve.js
@@ -0,0 +1,198 @@
+/**
+ * Socket.ioチャット
+ *
+ * @author M.Katsube 
+ */
+
+//-----------------------------------------------
+// モジュール
+//-----------------------------------------------
+const crypto = require("crypto");
+const app  = require("express")();
+const http = require("http").createServer(app);
+const io   = require("socket.io")(http);
+
+//-----------------------------------------------
+// 定数
+//-----------------------------------------------
+// HTMLやJSなどを配置するディレクトリ
+const DOCUMENT_ROOT = __dirname + "/public";
+
+// トークンを作成する際の秘密鍵
+const SECRET_TOKEN = "abcdefghijklmn12345";
+
+//-----------------------------------------------
+// グローバル変数
+//-----------------------------------------------
+// チャット参加者一覧
+const MEMBER = {};
+  // ↑以下のような内容のデータが入る
+  // {
+  //   "socket.id": {token:"abcd", name:"foo", count:1},
+  //   "socket.id": {token:"efgh", name:"bar", count:2}
+  // }
+
+// チャット延べ参加者数
+let MEMBER_COUNT = 1;
+
+//-----------------------------------------------
+// HTTPサーバ (express)
+//-----------------------------------------------
+/**
+ * "/"にアクセスがあったらindex.htmlを返却
+ */
+app.get("/", (req, res)=>{
+  res.sendFile(DOCUMENT_ROOT + "/index.html");
+});
+/**
+ * その他のファイルへのアクセス
+ * (app.js, style.cssなど)
+ */
+app.get("/:file", (req, res)=>{
+  res.sendFile(DOCUMENT_ROOT + "/" + req.params.file);
+});
+
+
+//-----------------------------------------------
+// Socket.io
+//-----------------------------------------------
+/**
+ * [イベント] ユーザーが接続
+ */
+io.on("connection", (socket)=>{
+  //---------------------------------
+  // トークンを返却
+  //---------------------------------
+  (()=>{
+    // トークンを作成
+    const token = makeToken(socket.id);
+
+    // ユーザーリストに追加
+    MEMBER[socket.id] = {token: token, name:null, count:MEMBER_COUNT};
+    MEMBER_COUNT++;
+
+    // 本人にトークンを送付
+    io.to(socket.id).emit("token", {token:token});
+  })();
+
+  /**
+   * [イベント] 入室する
+   */
+  socket.on("join", (data)=>{
+    //--------------------------
+    // トークンが正しければ
+    //--------------------------
+    if( authToken(socket.id, data.token) ){
+      // 入室OK + 現在の入室者一覧を通知
+      const memberlist = getMemberList();
+      io.to(socket.id).emit("join-result", {status: true, list: memberlist});
+
+      // メンバー一覧に追加
+      MEMBER[socket.id].name = data.name;
+
+      // 入室通知
+      io.to(socket.id).emit("member-join", data);
+      socket.broadcast.emit("member-join", {name:data.name, token:MEMBER[socket.id].count});
+    }
+    //--------------------------
+    // トークンが誤っていた場合
+    //--------------------------
+    else{
+      // 本人にNG通知
+      io.to(socket.id).emit("join-result", {status: false});
+    }
+  });
+
+  /**
+   * [イベント] 発言を全員に中継
+   */
+  socket.on("post", (data)=>{
+    //--------------------------
+    // トークンが正しければ
+    //--------------------------
+    if( authToken(socket.id, data.token) ){
+      // 本人に通知
+      io.to(socket.id).emit("member-post", data);
+
+      // 本人以外に通知
+      socket.broadcast.emit("member-post", {text:data.text, token:MEMBER[socket.id].count});
+    }
+
+    // トークンが誤っていた場合は無視する
+  });
+
+  /**
+   * [イベント] 退室する
+   */
+  socket.on("quit", (data)=>{
+    //--------------------------
+    // トークンが正しければ
+    //--------------------------
+    if( authToken(socket.id, data.token) ){
+      // 本人に通知
+      io.to(socket.id).emit("quit-result", {status: true});
+
+      // 本人以外に通知
+      socket.broadcast.emit("member-quit", {token:MEMBER[socket.id].count});
+
+      // 削除
+      delete MEMBER[socket.id];
+    }
+    //--------------------------
+    // トークンが誤っていた場合
+    //--------------------------
+    else{
+      // 本人にNG通知
+      io.to(socket.id).emit("quit-result", {status: false});
+    }
+  });
+
+});
+
+/**
+ * 3000番でサーバを起動する
+ */
+http.listen(3000, ()=>{
+  console.log("listening on *:3000");
+});
+
+
+/**
+ * トークンを作成する
+ *
+ * @param  {string} id - socket.id
+ * @return {string}
+ */
+function makeToken(id){
+  const str = SECRET_TOKEN + id;
+  return( crypto.createHash("sha1").update(str).digest('hex') );
+}
+
+/**
+ * 本人からの通信か確認する
+ *
+ * @param {string} socketid
+ * @param {string} token
+ * @return {boolean}
+ */
+function authToken(socketid, token){
+  return(
+    (socketid in MEMBER) && (token === MEMBER[socketid].token)
+  );
+}
+
+/**
+ * メンバー一覧を作成する
+ *
+ * @return {array}
+ */
+function getMemberList(){
+  const list = [];
+  for( let key in MEMBER ){
+    const cur = MEMBER[key];
+    if( cur.name !== null ){
+      list.push({token:cur.count, name:cur.name});
+    }
+  }
+  return(list);
+}
